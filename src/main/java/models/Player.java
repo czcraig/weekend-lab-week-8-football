944a93842f1017371f90262d080c800e6e4f107b
Chris Craig <ctc2@live.co.uk>
2018-07-13 12:23:04 +0100

    completed player class

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,4 +1,73 @@
 package models;
 
 public class Player {
+
+    private int id;
+    private String name;
+    private int age;
+    private String poisition;
+    private Manager manager;
+
+    public Player(){}
+
+    public Player(String name, int age, String poisition, Manager manager) {
+        this.name = name;
+        this.age = age;
+        this.poisition = poisition;
+        this.manager = manager;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPoisition() {
+        return poisition;
+    }
+
+    public void setPoisition(String poisition) {
+        this.poisition = poisition;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
