ccfece3c5049353c3f5af22d34d53caf586dcada
Chris Craig <ctc2@live.co.uk>
2018-07-13 12:26:26 +0100

    completed competition class

--- src/main/java/models/Competition.java
+++ src/main/java/models/Competition.java
@@ -1,4 +1,40 @@
 package models;
 
+import java.util.List;
+
 public class Competition {
+
+    private int id;
+    private String name;
+    private List<Team> teams;
+
+    public Competition(String name, List<Team> teams) {
+        this.name = name;
+        this.teams = teams;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
