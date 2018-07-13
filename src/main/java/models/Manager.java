c71d01d3230effdef7f50b27031d7a52a55e1d8f
Chris Craig <ctc2@live.co.uk>
2018-07-13 12:13:00 +0100

    completed manager class

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,51 @@
 package models;
 
 public class Manager {
+
+    private int id;
+    private String name;
+    private int age;
+    private Team team;
+
+    public Manager(String name, int age, Team team) {
+
+        this.name = name;
+        this.age = age;
+        this.team = team;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+
 }
